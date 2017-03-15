a962048ba9dd725ee469e3e9d1db9df9cc1087d4
fdm1 <frankmassi@gmail.com>
2017-03-15 18:33:21 +0000

    week 1 example down, setup for week 1 wikipedia assignment

---
+++ coursera/week_1/download_wikipedia_data.sh
@@ -0,0 +1,5 @@
+if [ ! -f wikipedia/src/main/resources/wikipedia/wikipedia.dat ]; then
+  mkdir -p wikipedia/src/main/resources/wikipedia
+  cd wikipedia/src/main/resources/wikipedia
+  wget http://alaska.epfl.ch/~dockermoocs/bigdata/wikipedia.dat
+fi
