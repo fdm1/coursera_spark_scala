c05c88a71ac10c9912cbdf9c8cf5e9fd992d4869
fdm1 <frankmassi@gmail.com>
2017-03-17 12:49:30 +0000

    week1 done

--- coursera/week_1/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ coursera/week_1/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -76,8 +76,16 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = ???
+  def rankLangsReduceByKey(langs: List[String], rdd: RDD[WikipediaArticle]): List[(String, Int)] = {
 
+    rdd.flatMap(article => article.text.split(" ")
+                                  .intersect(langs)
+                                  .map(lang => (lang, 1)))
+       .reduceByKey(_ + _)
+       .collect
+       .toList
+       .sortBy(- _._2)
+  }
   def main(args: Array[String]) {
 
     /* Languages ranked according to (1) */
