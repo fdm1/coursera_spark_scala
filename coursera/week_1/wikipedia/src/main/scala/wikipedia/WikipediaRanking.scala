ea0affb0ecdecd3312c99d304cfb62c2ea105a60
Frank Massi <frankmassi@gmail.com>
2017-03-20 08:53:44 -0500

    fixed wikipedia makeIndex

--- coursera/week_1/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
+++ coursera/week_1/wikipedia/src/main/scala/wikipedia/WikipediaRanking.scala
@@ -46,13 +46,12 @@ object WikipediaRanking {
          .sortBy(- _._2)
   }
 
-// wikiRdd.flatMap(article => (article.text.split(" ").intersect(langs), article))
   /* Compute an inverted index of the set of articles, mapping each language
    * to the Wikipedia pages in which it occurs.
    */
-  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = ???
-  //   rdd.map(article => (article.text.split(" ").intersect(langs), article))
-  // }
+  def makeIndex(langs: List[String], rdd: RDD[WikipediaArticle]): RDD[(String, Iterable[WikipediaArticle])] = {
+    rdd.flatMap(article => article.text.split(" ").intersect(langs).map(lang => (lang, article))).groupByKey
+  }
 
   /* (2) Compute the language ranking again, but now using the inverted index. Can you notice
    *     a performance improvement?
@@ -60,9 +59,9 @@ object WikipediaRanking {
    *   Note: this operation is long-running. It can potentially run for
    *   several seconds.
    */
-  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = ???
-
-  // }
+  def rankLangsUsingIndex(index: RDD[(String, Iterable[WikipediaArticle])]): List[(String, Int)] = {
+    ???
+  }
 
   /* (3) Use `reduceByKey` so that the computation of the index and the ranking are combined.
    *     Can you notice an improvement in performance compared to measuring *both* the computation of the index
