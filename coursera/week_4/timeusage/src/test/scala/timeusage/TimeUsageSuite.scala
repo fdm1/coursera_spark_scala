009c14c1aa9edef5e98d6e8cc4e1fd8d70716107
fdm1 <frankmassi@gmail.com>
2017-04-07 22:07:49 +0000

    add some tests

--- coursera/week_4/timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ coursera/week_4/timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -19,13 +19,46 @@ import TimeUsage._
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
     test("'read' can read raw data") {
       val (columns, initDf) = read("/timeusage/atussum.csv")
+      val firstRow = initDf.first
+      val expectedFirstRowList =
+        List("\"1\"", 60.asInstanceOf[Double]
+                    , 2.asInstanceOf[Double]
+                    , 2.asInstanceOf[Double]
+                    , 0.asInstanceOf[Double]
+                    , 1.asInstanceOf[Double]
+                    , 1.asInstanceOf[Double]
+                    , 3.asInstanceOf[Double]
+                    , 0.asInstanceOf[Double]
+                    , 5.asInstanceOf[Double]
+                    , 6.asInstanceOf[Double])
+      assert(firstRow ==  Row.fromSeq(expectedFirstRowList))
+    }
+
+    test("'dfSchema' generates correct schema") {
+      val (columns, initDf) = read("/timeusage/atussum.csv")
       assert(columns == List("tucaseid","teage","telfs","tesex","tfoo","t01","t02","t03","t18","t05","t07"))
-//    assert contents of initDf.first
-//    assert schema of initDf
+
+      val schema = StructType(Array(StructField("tucaseid",StringType,false),
+                                    StructField("teage",DoubleType,false),
+                                    StructField("telfs",DoubleType,false),
+                                    StructField("tesex",DoubleType,false),
+                                    StructField("tfoo",DoubleType,false),
+                                    StructField("t01",DoubleType,false),
+                                    StructField("t02",DoubleType,false),
+                                    StructField("t03",DoubleType,false),
+                                    StructField("t18",DoubleType,false),
+                                    StructField("t05",DoubleType,false),
+                                    StructField("t07",DoubleType,false)))
+
+      assert(dfSchema(columns) == schema)
     }
 
     test("'row' keeps first value as String and all other values to Double") {
-//    assert some stuff
+      val vals = List("1","2","3","4")
+      val res = Row.fromSeq(List("1", 2.asInstanceOf[Double]
+                                    , 3.asInstanceOf[Double]
+                                    , 4.asInstanceOf[Double]))
+      assert(row(vals) == res)
     }
 
 }
